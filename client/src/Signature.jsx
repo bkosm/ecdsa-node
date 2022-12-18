7847591f07a57540568227ad2b686e64f551a50b
bartosz.kosmala <bartosz.kosmala@olx.pl>
2022-12-18 16:11:51 +0100

    final signature

--- client/src/Signature.jsx
+++ client/src/Signature.jsx
@@ -1,29 +1,77 @@
-import server from "./server";
 import { useState } from "react";
+import { hashMessage, isSigned, pipe, signMessage, toHex } from "./utils";
 
-export default function Signature({ keys }) {
+const pipeVal = (setter) => (evt) => setter(evt.target.value);
+
+export default function Signature({}) {
+  const [message, setMessage] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
+  const [publicKey, setPublicKey] = useState("");
   const [signature, setSignature] = useState("");
 
-  function onGenerate(evt) {
-    console.log(`Generate signature ${evt}`);
-    setSignature("signature")
+  const [recoveryBit, setRecoveryBit] = useState(0);
+
+  const messageHash = pipe(hashMessage(message), toHex);
+  const hasError = !isSigned(signature, messageHash, publicKey);
+
+
+  async function onGenerate(_) {
+    if (!message || !privateKey) {
+      return;
+    }
+
+    const [actual, recoveryBit] = await signMessage(message, privateKey);
+
+    setSignature(toHex(actual));
+    setRecoveryBit(recoveryBit);
   }
 
   return (
-    <div className="container wallet">
+    <div className={`container wallet ${hasError && "error"}`}>
       <h2>Signature creator</h2>
 
       <label>
-        Message
-        <input disabled={true} value={"hello"} readOnly></input>
+        Private key (for signing)
+        <input value={privateKey} onChange={pipeVal(setPrivateKey)} />
       </label>
 
       <label>
-        Signature
+        Public key (for verification check)
+        <input value={publicKey} onChange={pipeVal(setPublicKey)} />
+      </label>
+
+      <hr/>
+
+      <label>
+        Message
+        <input value={message} onChange={pipeVal(setMessage)} />
+      </label>
+
+      <label>
+        Message hash
         <input
           disabled={true}
-          placeholder="Generated signature"
+          value={messageHash}
+          onChange={pipeVal(setMessage)}
+          readOnly
+        />
+      </label>
+
+      <hr/>
+
+      <label>
+      <b>Actual signature</b>
+        <input
           value={signature}
+          onChange={pipeVal(setSignature)}
+        ></input>
+      </label>
+
+      <label>
+        <b>Recovery bit</b>
+        <input
+          disabled={true}
+          value={recoveryBit}
           readOnly
         ></input>
       </label>
